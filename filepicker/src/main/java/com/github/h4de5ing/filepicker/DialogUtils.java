82be6b0eeb645bf93de26e9331365a771694cf7d
h4de5ing <lex109@qq.com>
2021-11-04 17:10:05 +0800

    Thu Nov  4 17:10:03     2021

---
+++ filepicker/src/main/java/com/github/h4de5ing/filepicker/DialogUtils.java
@@ -0,0 +1,31 @@
+package com.github.h4de5ing.filepicker;
+
+import android.content.Context;
+import android.os.Environment;
+
+import com.github.h4de5ing.filepicker.model.DialogConfigs;
+import com.github.h4de5ing.filepicker.model.DialogProperties;
+import com.github.h4de5ing.filepicker.view.FilePickerDialog;
+
+import java.io.File;
+
+public class DialogUtils {
+    public interface DialogSelection {
+        void onSelectedFilePaths(String[] files);
+    }
+
+    public static void select_file(Context context, String title, final DialogSelection dialogSelection) {
+        String default_dir = Environment.getExternalStorageDirectory().getAbsolutePath();
+        DialogProperties properties = new DialogProperties();
+        properties.selection_mode = DialogConfigs.SINGLE_MODE;
+        properties.selection_type = DialogConfigs.FILE_SELECT;
+        properties.root = new File(default_dir);
+        properties.error_dir = new File(default_dir);
+        properties.offset = new File(default_dir);
+        properties.extensions = null;
+        FilePickerDialog dialog = new FilePickerDialog(context, properties);
+        dialog.setTitle(title);
+        dialog.show();
+        dialog.setDialogSelectionListener(files -> dialogSelection.onSelectedFilePaths(files));
+    }
+}
