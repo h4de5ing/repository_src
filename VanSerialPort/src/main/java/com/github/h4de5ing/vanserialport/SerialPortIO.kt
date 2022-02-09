79cae900f8ba5ef6be616541519a6d227bcf6094
h4de5ing <lex109@qq.com>
2022-02-09 18:12:03 +0800

    Wed Feb  9 18:12:01     2022

--- VanSerialPort/src/main/java/com/github/h4de5ing/vanserialport/SerialPortIO.kt
+++ VanSerialPort/src/main/java/com/github/h4de5ing/vanserialport/SerialPortIO.kt
@@ -1,63 +1,50 @@
-package com.github.h4de5ing.serialportlib
+package com.github.h4de5ing.vanserialport
 
-import java.io.InputStream
-import java.io.OutputStream
+import com.van.uart.LastError
+import com.van.uart.UartManager
+import com.van.uart.UartManager.BaudRate
 import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
 
-/**
- * Created by luowei on 2017/9/14.
- * https://github.com/luv135/AsyncIO/blob/fa64145b48f1c925bc428961f6c05b9bd72ec439/baseiolibrary/src/main/java/com/unistrong/luowei/factorysuite/pc/serial/BaseIO.kt
- *
- */
-abstract class BaseIO {
-
+object SerialPortIO {
     private var isRun: Boolean = false
     private val executor = ThreadPoolExecutor(3, 10, 5, TimeUnit.SECONDS, LinkedBlockingQueue())
-
-    fun start(
-        inputStream: InputStream,
-        outputStream: OutputStream,
-        callback: (buffer: ByteArray, size: Int) -> Unit
-    ) {
-        isRun = true
-        readThread = startReadThread(inputStream, callback)
-        writeThread = startWriteThread(outputStream, readThread!!)
-    }
-
+    private var serialPort: UartManager? = null
     private var readThread: ReadThread? = null
     private var writeThread: WriteThread? = null
-    open fun stop() {
-        isRun = false
-        readThread?.close()
-        readThread = null
-        writeThread?.close()
-        writeThread = null
-
+    fun start(name: String, baud: Int, callback: (buffer: ByteArray, size: Int) -> Unit) {
+        try {
+            serialPort = UartManager()
+            serialPort?.open(name, getBaudRate(baud))
+            isRun = true
+            readThread = startReadThread(callback)
+            writeThread = startWriteThread(readThread!!)
+        } catch (e: Exception) {
+            var result = "${e.message}".toByteArray()
+            callback(result, result.size)
+            e.printStackTrace()
+        }
     }
 
+    fun write(data: ByteArray): Boolean = (writeThread?.write(Packet(data)) == true)
     fun write(packet: Packet): Boolean = isRun && (writeThread?.write(packet) == true)
-    fun write(data: ByteArray): Boolean = isRun && (writeThread?.write(Packet(data)) == true)
-
-    private fun startWriteThread(outputStream: OutputStream, readThread: ReadThread): WriteThread {
-        val writeThread = WriteThread(outputStream, readThread)
-        executor.execute(writeThread)
-        return writeThread
-    }
-
     private fun startReadThread(
-        inputStream: InputStream,
         callback: (buffer: ByteArray, size: Int) -> Unit
     ): ReadThread {
-        val readThread = ReadThread(inputStream, callback)
+        val readThread = ReadThread(callback)
         executor.execute(readThread)
         return readThread
     }
 
-    inner class WriteThread(
-        private val outputStream: OutputStream,
+    private fun startWriteThread(readThread: ReadThread): WriteThread {
+        val writeThread = WriteThread(readThread)
+        executor.execute(writeThread)
+        return writeThread
+    }
+
+    private class WriteThread(
         private val readThread: ReadThread
     ) : Thread() {
         private val queen = LinkedList<Packet>()
@@ -76,21 +63,27 @@ abstract class BaseIO {
                 }
                 val poll = queen.poll()
                 try {
-                    write(poll.buffer)
+                    (poll.buffer)
                     var buffer: ByteArray? = null
                     if (poll.callback != null) {
                         buffer = readThread.get(2000)
                     }
                     poll.callback?.invoke(buffer != null, buffer ?: poll.buffer)
                 } catch (e: Exception) {
-                    this@BaseIO.stop()
+                    //e.printStackTrace()
                 }
             }
         }
 
         @Synchronized
         private fun write(buffer: ByteArray) {
-            outputStream.write(buffer)
+            if (serialPort != null && serialPort?.isOpen == true) {
+                try {
+                    serialPort?.write(buffer, buffer.size)
+                } catch (lastError: LastError) {
+                    lastError.printStackTrace()
+                }
+            }
         }
 
         fun write(packet: Packet): Boolean {
@@ -106,29 +99,22 @@ abstract class BaseIO {
         }
 
         fun close() {
-            write(Packet(ByteArray(0)))
+            write(ByteArray(0))
             interrupt()
         }
     }
 
-    class Packet(
-        val buffer: ByteArray,
-        val callback: ((success: Boolean, buffer: ByteArray) -> Unit)? = null
-    )
 
-
-    inner class ReadThread(
-        private val inputStream: InputStream,
+    private class ReadThread(
         private val callback: (buffer: ByteArray, size: Int) -> Unit
     ) : Thread() {
-        private val readBuffer = ByteArray(1024)
+        private val readBuffer = ByteArray(2048)
         private var readSize = 0
         private val objecz = Object()
-
         override fun run() {
-            while (isRun) {
+            while (serialPort!!.isOpen) {
                 try {
-                    readSize = inputStream.read(readBuffer)
+                    readSize = serialPort?.read(readBuffer, readBuffer.size, 50, 1)!!
                     if (readSize > 0) {
                         synchronized(objecz) {
                             objecz.notify()
@@ -136,7 +122,10 @@ abstract class BaseIO {
                         callback.invoke(readBuffer, readSize)
                     }
                 } catch (e: Exception) {
-                    this@BaseIO.stop()
+                    isRun = false
+                    readThread?.close()
+                    readThread = null
+                    e.printStackTrace()
                 }
             }
         }
@@ -161,5 +150,40 @@ abstract class BaseIO {
         fun close() {
             interrupt()
         }
+    }
+
+    class Packet(
+        val buffer: ByteArray,
+        val callback: ((success: Boolean, buffer: ByteArray) -> Unit)? = null
+    )
+
+    fun stop() {
+        isRun = false
+        readThread?.close()
+        readThread = null
+        writeThread?.close()
+        writeThread = null
+        serialPort?.close()
+        serialPort = null
+    }
+
+    fun getBaudRate(baudrate: Int): BaudRate? {
+        var value: BaudRate? = null
+        when (baudrate) {
+            9600 -> value = BaudRate.B9600
+            19200 -> value = BaudRate.B19200
+            57600 -> value = BaudRate.B57600
+            115200 -> value = BaudRate.B115200
+            230400 -> value = BaudRate.B230400
+            else -> {
+                value = BaudRate.B115200
+                Exception("not support baudrate [$baudrate]")
+            }
+        }
+        return value
+    }
+
+    fun getSupportBauds(): Array<Int> {
+        return arrayOf(1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200, 230400)
     }
 }
