d68a3effd48a4de3db6437411099043f74c91d4d
h4de5ing <lex109@qq.com>
2022-03-08 15:39:54 +0800

    优化Van串口工具

--- VanSerialPort/src/main/java/com/github/h4de5ing/vanserialport/SerialPortIO.kt
+++ VanSerialPort/src/main/java/com/github/h4de5ing/vanserialport/SerialPortIO.kt
@@ -1,9 +1,7 @@
 package com.github.h4de5ing.vanserialport
 
-import com.van.uart.LastError
 import com.van.uart.UartManager
 import com.van.uart.UartManager.BaudRate
-import java.util.*
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.ThreadPoolExecutor
 import java.util.concurrent.TimeUnit
@@ -13,7 +11,6 @@ object SerialPortIO {
     private val executor = ThreadPoolExecutor(3, 10, 5, TimeUnit.SECONDS, LinkedBlockingQueue())
     private var serialPort: UartManager? = null
     private var readThread: ReadThread? = null
-    private var writeThread: WriteThread? = null
     fun start(name: String, baud: Int, callback: (buffer: ByteArray, size: Int) -> Unit) {
         try {
             val devName: String = name.split("/dev/").toTypedArray()[1]
@@ -21,7 +18,6 @@ object SerialPortIO {
             serialPort?.open(devName, getBaudRate(baud))
             isRun = true
             readThread = startReadThread(callback)
-            writeThread = startWriteThread(readThread!!)
         } catch (e: Exception) {
             var result = "${e.message}".toByteArray()
             callback(result, result.size)
@@ -29,8 +25,7 @@ object SerialPortIO {
         }
     }
 
-    fun write(data: ByteArray): Boolean = (writeThread?.write(Packet(data)) == true)
-    fun write(packet: Packet): Boolean = isRun && (writeThread?.write(packet) == true)
+    fun write(data: ByteArray): Int? = serialPort?.write(data, data.size)
     private fun startReadThread(
         callback: (buffer: ByteArray, size: Int) -> Unit
     ): ReadThread {
@@ -38,73 +33,6 @@ object SerialPortIO {
         executor.execute(readThread)
         return readThread
     }
-
-    private fun startWriteThread(readThread: ReadThread): WriteThread {
-        val writeThread = WriteThread(readThread)
-        executor.execute(writeThread)
-        return writeThread
-    }
-
-    private class WriteThread(
-        private val readThread: ReadThread
-    ) : Thread() {
-        private val queen = LinkedList<Packet>()
-        private val objecz = Object()
-
-        init {
-            readThread.setWriteThread(this)
-        }
-
-        override fun run() {
-            while (isRun) {
-                while (queen.isEmpty()) {
-                    synchronized(objecz) {
-                        objecz.wait()
-                    }
-                }
-                val poll = queen.poll()
-                try {
-                    (poll.buffer)
-                    var buffer: ByteArray? = null
-                    if (poll.callback != null) {
-                        buffer = readThread.get(2000)
-                    }
-                    poll.callback?.invoke(buffer != null, buffer ?: poll.buffer)
-                } catch (e: Exception) {
-                    //e.printStackTrace()
-                }
-            }
-        }
-
-        @Synchronized
-        private fun write(buffer: ByteArray) {
-            if (serialPort != null && serialPort?.isOpen == true) {
-                try {
-                    serialPort?.write(buffer, buffer.size)
-                } catch (lastError: LastError) {
-                    lastError.printStackTrace()
-                }
-            }
-        }
-
-        fun write(packet: Packet): Boolean {
-            var rt = false
-            if (queen.size < 10) {
-                queen.offer(packet)
-                rt = true
-            }
-            synchronized(objecz) {
-                objecz.notify()
-            }
-            return rt
-        }
-
-        fun close() {
-            write(ByteArray(0))
-            interrupt()
-        }
-    }
-
 
     private class ReadThread(
         private val callback: (buffer: ByteArray, size: Int) -> Unit
@@ -131,39 +59,15 @@ object SerialPortIO {
             }
         }
 
-        fun get(timeout: Long): ByteArray? {
-            if (readSize <= 0) {
-                synchronized(objecz) {
-                    objecz.wait(timeout)
-                }
-            }
-            if (readSize > 0)
-                return readBuffer.copyOfRange(0, readSize)
-            return null
-        }
-
-        private lateinit var writeThread: WriteThread
-
-        fun setWriteThread(writeThread: WriteThread) {
-            this.writeThread = writeThread
-        }
-
         fun close() {
             interrupt()
         }
     }
 
-    class Packet(
-        val buffer: ByteArray,
-        val callback: ((success: Boolean, buffer: ByteArray) -> Unit)? = null
-    )
-
     fun stop() {
         isRun = false
         readThread?.close()
         readThread = null
-        writeThread?.close()
-        writeThread = null
         serialPort?.close()
         serialPort = null
     }
