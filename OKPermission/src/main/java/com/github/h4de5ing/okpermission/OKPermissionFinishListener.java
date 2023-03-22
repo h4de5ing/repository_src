5c65b351487c2b26dad6607f9f07b509703f44e6
h4de5ing <lex109@qq.com>
2023-03-22 16:41:49 +0800

    删除权限库

--- OKPermission/src/main/java/com/github/h4de5ing/okpermission/OKPermissionFinishListener.java
+++
@@ -1,6 +0,0 @@
-package com.github.h4de5ing.okpermission;
-
-//权限申请结束的监听，用于如果已经全部授权的回调
-public interface OKPermissionFinishListener {
-    void onOKPermissionFinish();
-}
--- OKPermission/src/main/java/com/github/h4de5ing/okpermission/OKPermissionKeyBackListener.java
+++
@@ -1,6 +0,0 @@
-package com.github.h4de5ing.okpermission;
-
-//权限提示对话框，点击返回键的监听
-public interface OKPermissionKeyBackListener {
-    void onKeyBackListener();
-}
