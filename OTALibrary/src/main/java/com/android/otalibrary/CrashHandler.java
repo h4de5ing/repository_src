a43bba2563df4c7fdbc73dfbb6879b8190d42f78
h4de5ing <lex109@qq.com>
2023-08-17 16:35:21 +0800

    Thu Aug 17 16:35:21     2023

--- OTALibrary/src/main/java/com/android/otalibrary/CrashHandler.java
+++ OTALibrary/src/main/java/com/android/otalibrary/CrashHandler.java
@@ -27,92 +27,57 @@ import java.util.Objects;
 
 
 public class CrashHandler implements Thread.UncaughtExceptionHandler {
-    // 系统默认的UncaughtException处理类
     private Thread.UncaughtExceptionHandler mDefaultHandler;
-    // CrashHandler实例
     @SuppressLint("StaticFieldLeak")
     private static final CrashHandler INSTANCE = new CrashHandler();
-    // 程序的Context对象
     private Context mContext;
-    // 用来存储设备信息和异常信息
     private final Map<String, String> appInformation = new HashMap<>();
-
-    // 用于格式化日期,作为日志文件名的一部分
     @SuppressLint("SimpleDateFormat")
     private final DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
     private File logDir;
 
-    /**
-     * 保证只有一个CrashHandler实例
-     */
     private CrashHandler() {
     }
 
-    /**
-     * 获取CrashHandler实例 ,单例模式
-     */
     public static CrashHandler getInstance() {
         return INSTANCE;
     }
 
-    /**
-     * 初始化
-     */
     public void init(Context context) {
         mContext = context;
         logDir = mContext.getExternalFilesDir("LOG");
-        // 获取系统默认的UncaughtException处理器  
         mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
-        // 设置该CrashHandler为程序的默认处理器  
         Thread.setDefaultUncaughtExceptionHandler(this);
     }
 
-    /**
-     * 当UncaughtException发生时会转入该函数来处理
-     */
     @Override
     public void uncaughtException(@NotNull Thread thread, @NotNull Throwable ex) {
         if (!handleException(ex) && mDefaultHandler != null) {
-            // 如果用户没有处理则让系统默认的异常处理器来处理  
             mDefaultHandler.uncaughtException(thread, ex);
         } else {
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException ignored) {
             }
-            // 退出程序  
             android.os.Process.killProcess(android.os.Process.myPid());
             System.exit(1);
         }
     }
 
-    /**
-     * d
-     * 自定义错误处理,收集错误信息 发送错误报告等操作均在此完成.
-     *
-     * @return true:如果处理了该异常信息;否则返回false.
-     */
+
     private boolean handleException(Throwable ex) {
-        if (ex == null) {
-            return false;
-        }
-        // 使用Toast来显示异常信息
+        if (ex == null) return false;
         new Thread(() -> {
             Looper.prepare();
             Toast.makeText(mContext, "很抱歉," + mContext.getPackageName() + "程序出现异常,正在收集日志，即将退出", Toast.LENGTH_SHORT)
                     .show();
             Looper.loop();
         }).start();
-        // 收集设备参数信息  
         collectDeviceInfo(mContext);
-        // 保存日志文件
         saveCrashInfo2File(ex);
         return true;
     }
 
-    /**
-     * 收集设备参数信息
-     */
     public void collectDeviceInfo(Context ctx) {
         try {
             PackageManager pm = ctx.getPackageManager();
@@ -135,9 +100,6 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         }
     }
 
-    /**
-     * 保存错误信息到文件中
-     */
     private void saveCrashInfo2File(Throwable ex) {
 
         StringBuilder sb = new StringBuilder();
@@ -161,7 +123,7 @@ public class CrashHandler implements Thread.UncaughtExceptionHandler {
         try {
             long timestamp = System.currentTimeMillis();
             String time = formatter.format(new Date());
-            String fileName = time + "-" + timestamp + ".log";
+            String fileName = time + "-" + timestamp + ".txt";
             if (Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)) {
                 File file = new File(logDir, fileName);
                 FileOutputStream fos = new FileOutputStream(file);
